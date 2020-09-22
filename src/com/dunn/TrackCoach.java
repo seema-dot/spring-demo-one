51ee871ca9e3ffefa8952893e00932b2c103e339
seema-dot <70341529+seema-dot@users.noreply.github.com>
2020-09-22 21:28:18 +0530

    Update TrackCoach.java

--- src/com/dunn/TrackCoach.java
+++ src/com/dunn/TrackCoach.java
@@ -6,7 +6,8 @@ public class TrackCoach implements Coach{
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
 		System.out.println("Hello How R you");
-		return "Spend some time on Track...Hello How are You";
+	
+		return "Spend some time on Track...Hello How are You I am Good...";
 		
 	}
 
