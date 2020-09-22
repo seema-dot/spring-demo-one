2ba49420598e6c69adc38c4b4b40d3006ed956cb
seema <ms.heer3003@gmail.com>
2020-09-22 21:14:26 +0530

    Modified locally

--- src/com/dunn/TrackCoach.java
+++ src/com/dunn/TrackCoach.java
@@ -5,7 +5,7 @@ public class TrackCoach implements Coach{
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
-		return "Spend some time on Track...Hello";
+		return "Spend some time on Track...Hello How are You";
 	}
 
 }
